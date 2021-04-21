58ab7839342e78838897255314e961afdde42db4
PRANITA <82086528+Pranita-03@users.noreply.github.com>
2021-04-21 16:02:05 +0530

    Add files via upload

---
+++ project_student.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: project
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `rollNo` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `fatherName` varchar(45) NOT NULL,
+  `motherName` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `contactNo` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `tenthUniversityName` varchar(45) NOT NULL,
+  `tenthPercentage` varchar(45) NOT NULL,
+  `tenthPassoutYear` varchar(45) NOT NULL,
+  `twelveUniversityName` varchar(45) NOT NULL,
+  `twelvePercentage` varchar(45) NOT NULL,
+  `twelvePassoutYear` varchar(45) NOT NULL,
+  `graduationUniversityName` varchar(45) NOT NULL,
+  `graduationPercentage` varchar(45) NOT NULL,
+  `graduationPassoutYear` varchar(45) NOT NULL,
+  `Address` varchar(45) NOT NULL,
+  `marks` varchar(45) NOT NULL,
+  PRIMARY KEY (`rollNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'harshada','sanjay','madhura','Female','9922337718','Harsha@gmail.com','bsjk','78','2011','bnjn','56','2016','jbhbn ','86','2020','Pune','4'),(22,'pranita','deepak','memane','Female','9874561230','pranita@gmail.com','mit','86','2017','mit','75','2018','viit','85','2020','pune','0');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-21 16:00:07
